f5b6c36a971d0aa32d33767ec2d18c87c1198d97
MURATAMIKU <mikkle0817@gmail.com>
2018-01-12 13:26:26 +0900

    PTra01_03~07まで完了。

--- Practice/src/practice01/PTra01_07.java
+++ Practice/src/practice01/PTra01_07.java
@@ -22,16 +22,25 @@ public class PTra01_07 {
 		// ★ それぞれの型変数に、値を代入してください
 		// ※ 値は、好きな数字を入力してください
 
-
-
-
-
-
-
-
+		bo = true;
+		by = 10;
+		s = 10;
+		c = '嵐';
+		i = 100;
+		l = 1000L;
+		f = 3.14F;
+		d = 3.39;
 
 		// ★ それぞれの変数の中身を出力してください
 
+		System.out.println(bo);
+		System.out.println(by);
+		System.out.println(s);
+		System.out.println(c);
+		System.out.println(i);
+		System.out.println(l);
+		System.out.println(f);
+		System.out.println(d);
 
 
 
